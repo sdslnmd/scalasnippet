e355331f59145590500348b4db2ab19101dacf3a
sunluning <sunluning@meituan.com>
2017-11-23 19:18:23 +0800

    add implicitTest

--- src/main/scala/akkastream/AkkaStreamGraphTest.scala
+++ src/main/scala/akkastream/AkkaStreamGraphTest.scala
@@ -1,8 +1,10 @@
 package akkastream
 
-import akka.NotUsed
-import akka.stream.ClosedShape
-import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Sink, Source}
+import akka.{Done, NotUsed}
+import akka.stream.{ClosedShape, KillSwitches, UniqueKillSwitch}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Keep, RunnableGraph, Sink, Source}
+
+import scala.concurrent.Future
 
 final case class Auther(handler: String)
 
@@ -23,21 +25,44 @@ final case class Tweet(auther: Auther, timestamp: Long, body: String) {
 
 object AkkaStreamGraphTest {
 
-//  val writeAuthors: Sink[Auther, NotUsed] = ???
-//  val writeHashtags: Sink[Hashtag, NotUsed] = ???
-//  val tweets: Source[Tweet, NotUsed] = ???
-//
-//  private val graph: RunnableGraph[NotUsed] = RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
-//    import GraphDSL.Implicits._
-//
-//    val bcast = b.add(Broadcast[Tweet](2))
-//    tweets ~> bcast.in
-//    bcast.out(0) ~> Flow[Tweet].map(_.auther) ~> writeAuthors
-//    bcast.out(1) ~> Flow[Tweet].mapConcat(_.hashtags.toList) ~> writeHashtags
-//
-//    ClosedShape
-//  })
-//
-//  graph.run()
+  val writeAuthors: Sink[Auther, NotUsed] = ???
+  val writeHashtags: Sink[Hashtag, NotUsed] = ???
+  val tweets: Source[Tweet, NotUsed] = ???
+
+  private val graph: RunnableGraph[NotUsed] =
+    RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
+      import GraphDSL.Implicits._
+
+      val bcast = b.add(Broadcast[Tweet](2))
+      tweets ~> bcast.in
+      bcast.out(0) ~> Flow[Tweet].map(_.auther) ~> writeAuthors
+      bcast.out(1) ~> Flow[Tweet].mapConcat(_.hashtags.toList) ~> writeHashtags
+
+      ClosedShape
+    })
+
+  //返回值不同
+  private val used: NotUsed = graph.run()
+
+}
+
+object AkkaStreamKillSwitch {
+
+  // 关注 toMat 方法
+  private val helloStream: RunnableGraph[(UniqueKillSwitch, Future[Done])] = Source
+    .single("hello world")
+    .map(_.toUpperCase)
+    .viaMat(KillSwitches.single)(Keep.right)
+    .toMat(Sink.foreach(println))(Keep.both)
+
+
+  private val (killSwitch,doneF): (UniqueKillSwitch, Future[Done]) = helloStream.run()
+
+  killSwitch.shutdown()
+  // or
+  killSwitch.abort(new Exception("Exception from KillSwitch"))
+
+
+
 
 }
