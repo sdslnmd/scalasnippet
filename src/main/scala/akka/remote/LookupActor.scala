c82d2b24e82c73d617c6ae4adf71dae0bffb52ba
sunluning <sunluning@meituan.com>
2017-12-05 22:58:08 +0800

    akka cluster

--- src/main/scala/akka/remote/LookupActor.scala
+++ src/main/scala/akka/remote/LookupActor.scala
@@ -1,22 +1,22 @@
-package akka.remote
-import scala.concurrent.duration._
-import akka.actor.{Actor, ActorIdentity, ReceiveTimeout}
-import akka.remote.ContainerFormats.ActorIdentity
-
-class LookupActor(path: String) extends Actor {
-
-  sendIdentifyRequest()
-
-  def sendIdentifyRequest(): Unit = {
-    context.actorSelection(path) ! identity(path)
-    import context.dispatcher
-    context.system.scheduler.scheduleOnce(3.second, self, ReceiveTimeout)
-  }
-
-  def identifying:Actor.Receive={
-//    case ActorIdentity('')
-
-  }
-
-  override def receive = ???
-}
+//package akka.remote
+//import scala.concurrent.duration._
+//import akka.actor.{Actor, ActorIdentity, ReceiveTimeout}
+//import akka.remote.ContainerFormats.ActorIdentity
+//
+//class LookupActor(path: String) extends Actor {
+//
+//  sendIdentifyRequest()
+//
+//  def sendIdentifyRequest(): Unit = {
+////    context.actorSelection(path) ! identity(path)
+////    import context.dispatcher
+////    context.system.scheduler.scheduleOnce(3.second, self, ReceiveTimeout)
+//  }
+//
+//  def identifying:Actor.Receive={
+////    case ActorIdentity('')
+//
+//  }
+//
+//  override def receive = ???
+//}
--- src/main/scala/collection/CollectionTest.scala
+++ src/main/scala/collection/CollectionTest.scala
@@ -2,7 +2,20 @@ package collection
 
 object CollectionTest {
 
+  def trans(m: Map[String, Map[String, List[String]]]): List[(String, String, String)] = {
+    for {
+      (k, m2) <- m
+      (k2, list) <- m2
+      item <- list
+    } yield (k, k2, item)
+  }.toList
+
   def main(args: Array[String]): Unit = {
+
+    val list = trans(
+      Map("a" -> Map("b" -> List("1", "2")), "c" -> Map("d" -> List("3", "4"))))
+
+    println("trans list to " + list)
 
     val s = Seq(1, 2, 3)
 
@@ -17,13 +30,12 @@ object CollectionTest {
     }
 
     val tuples = List(1, 2, 3).flatMap(x => Some('x').map(y => (x, y)))
-    println(tuples)
+    println(list)
 
-    val tuples2 = for{
-      x <- List(1,2,3)
+    val tuples2 = for {
+      x <- List(1, 2, 3)
       y <- "x"
-    }yield(x,y)
-
+    } yield (x, y)
 
     println("tuples2 123123123")
 
